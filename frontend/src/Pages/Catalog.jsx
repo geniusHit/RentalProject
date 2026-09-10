ca1747010389af20254c4ec36c0531c851db10ae
geniusHit <rohitthakur792002@gmail.com>
2026-09-10 20:51:35 +0530

    Vercel url change for api

--- frontend/src/Pages/Catalog.jsx
+++ frontend/src/Pages/Catalog.jsx
@@ -5,11 +5,8 @@ import NavBar from '../Components/NavBar';
 import Footer from '../Components/Footer';
 import { useForm } from "react-hook-form"
 import { jwtDecode } from "jwt-decode";
-const API_URL = import.meta.env.VERCEL_API_URL;
 
 const Catalog = () => {
-    console.log("API_URL : ", API_URL)
-
     const [products, setProducts] = useState([])
     const [itemMessage, setItemMessage] = useState("")
     const [showMessage, setShowMessage] = useState(false)
@@ -21,7 +18,7 @@ const Catalog = () => {
     const navigate = useNavigate()
 
     const getProducts = async () => {
-        const response = await fetch(`https://localhost:8000/get-products`)
+        const response = await fetch(`https://rental-project.vercel.app/get-products`)
         const result = await response.json()
         setProducts(result)
     }
