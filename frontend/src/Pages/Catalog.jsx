04d82b8454216b4cc7299b9de92511d48e0a2b96
geniusHit <rohitthakur792002@gmail.com>
2026-09-10 20:42:39 +0530

    Vercel url change for api

--- frontend/src/Pages/Catalog.jsx
+++ frontend/src/Pages/Catalog.jsx
@@ -21,7 +21,7 @@ const Catalog = () => {
     const navigate = useNavigate()
 
     const getProducts = async () => {
-        const response = await fetch(`https://rental-project-opal.vercel.app/get-products`)
+        const response = await fetch(`https://localhost:8000/get-products`)
         const result = await response.json()
         setProducts(result)
     }
