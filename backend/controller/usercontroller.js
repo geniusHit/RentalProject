5f35edd52a0deefe7940953dcb4250ce065adbdd
geniusHit <rohitthakur792002@gmail.com>
2026-09-11 13:33:08 +0530

    Vercel url change for api

--- backend/controller/usercontroller.js
+++ backend/controller/usercontroller.js
@@ -359,7 +359,7 @@ exports.createTestPaymentLink = async (req, res) => {
             send_email: false
         },
         link_meta: {
-            return_url: `http://localhost:5173/catalog`
+            return_url: `https://rental-project.vercel.app/catalog`
         }
     };
 
