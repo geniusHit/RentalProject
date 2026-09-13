6d345ebf403e8493c21a27373176c2d43e786d71
geniusHit <rohitthakur792002@gmail.com>
2026-09-13 16:27:33 +0530

    Vercel correction...

--- backend/index.js
+++ backend/index.js
@@ -7,16 +7,15 @@ const path = require("path");
 const router = require("./routes/router.js")
 const port = process.env.PORT
 
-// app.use(cors())
-app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://rental-project-opal.vercel.app"
-    ],
-    credentials: true
-}));
+app.use(cors())
+// app.use(cors({
+//     origin: [
+//         "http://localhost:5173",
+//         "https://rental-project-opal.vercel.app"
+//     ],
+//     credentials: true
+// }));
 app.use(express.json())
-// app.use(express.urlencoded({ extended: true }))
 app.use(router)
 app.use("/uploads", express.static("uploads"));
 
