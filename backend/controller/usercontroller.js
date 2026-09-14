542de77cc7a216f554b06fa7e18231b14cb11d27
geniusHit <rohitthakur792002@gmail.com>
2026-09-14 20:12:28 +0530

    Vercel correction...

--- backend/controller/usercontroller.js
+++ backend/controller/usercontroller.js
@@ -372,7 +372,7 @@ exports.createTestPaymentLink = async (req, res) => {
             send_email: false
         },
         link_meta: {
-            return_url: `https://rental-project-backend.vercel.app/catalog`
+            return_url: `https://rental-project-sigma.vercel.app/catalog`
         }
     };
 
