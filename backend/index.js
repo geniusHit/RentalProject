3a7d06bf6d89e10c9e655c7aa11b65a080f21f72
geniusHit <rohitthakur792002@gmail.com>
2026-09-14 17:24:12 +0530

    Vercel correction...

--- backend/index.js
+++ backend/index.js
@@ -4,18 +4,41 @@ const connection = require("./connection/database.js");
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 
+const corsOptions = {
+    origin: [
+        "http://localhost:5173",
+        "https://rental-project-94ezx9uff-geniushits-projects.vercel.app",
+        "https://rental-project-backend.vercel.app",
+        "https://rental-project.vercel.app"
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+};
+
 app.use(cors());
+// app.options("*", cors(corsOptions));
+
 app.use(express.json());
+app.use(cookieParser())
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Rental backend is working"
+    });
+});
+
+const router = require("./routes/router.js");
+app.use("/", router);
 
 app.use("/uploads",
     express.static(path.join(__dirname, "uploads"))
 );
-
-const router = require("./routes/router.js");
-
-app.use("/", router);
 
 module.exports = app;
 
