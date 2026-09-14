11d0a6eb0af02bd9ad0d32ca955ab72168f93465
geniusHit <rohitthakur792002@gmail.com>
2026-09-14 12:38:51 +0530

    Vercel correction...

--- frontend/src/Pages/MyRentalItems.jsx
+++ frontend/src/Pages/MyRentalItems.jsx
@@ -72,9 +72,9 @@ const MyRentalItems = () => {
 
             <h4 className='heading'>My Rental Items</h4>
 
-            <div className='products'>
+            <div className={products.length>0?`products`: ""}>
                 {
-                    products.map((prod, index) => {
+                    products.length > 0 ? products.map((prod, index) => {
                         return <div className='product' key={index}>
                             <div className='img' style={{
                                 backgroundImage: `url(https://rental-project-backend.vercel.app/uploads/${prod.imageNames[0]})`
@@ -85,6 +85,12 @@ const MyRentalItems = () => {
                             </div>
                         </div>
                     })
+                        :
+                        <div className='spinner-box'>
+                            <div className="spinner-border" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </div>
+                        </div>
                 }
             </div>
 
