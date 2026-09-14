574ab7c408ce80c2ed1d3b09a772828fe3aa9710
geniusHit <rohitthakur792002@gmail.com>
2026-09-14 14:23:36 +0530

    Vercel correction...

--- backend/controller/usercontroller.js
+++ backend/controller/usercontroller.js
@@ -129,6 +129,7 @@ exports.addProduct = async (req, res) => {
 }
 
 exports.getProducts = async (req, res) => {
+    console.log("SECRET : ", SECRET)
     const result = await productsModel.find({})
 
     res.status(200).json(result)
