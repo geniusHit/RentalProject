188439f3517520557511f78d987106690f240278
geniusHit <rohitthakur792002@gmail.com>
2026-09-15 19:26:38 +0530

    Vercel first push

--- backend/index.js
+++ backend/index.js
@@ -5,32 +5,11 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-
 const app = express();
 
-const corsOptions = {
-    origin: [
-        "http://localhost:5173",
-        "https://rental-project-94ezx9uff-geniushits-projects.vercel.app",
-        "https://rental-project-backend.vercel.app",
-        "https://rental-project.vercel.app"
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-};
-
 app.use(cors());
-
 app.use(express.json());
 app.use(cookieParser())
-
-// app.get("/", (req, res) => {
-//     res.status(200).json({
-//         success: true,
-//         message: "Rental backend is working"
-//     });
-// });
 
 const router = require("./routes/router.js");
 app.use("/", router);
