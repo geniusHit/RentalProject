c2700e6f76c067cd4741ef930392fa23d7b541c5
geniusHit <rohitthakur792002@gmail.com>
2026-09-15 18:18:24 +0530

    Vercel first push

--- backend/controller/usercontroller.js
+++ backend/controller/usercontroller.js
@@ -433,7 +433,7 @@ exports.createTestPaymentLink = async (req, res) => {
             send_email: false
         },
         link_meta: {
-            return_url: `https://rental-project-sigma.vercel.app/catalog`
+            return_url: `https://rental-project-seven.vercel.app//catalog`
         }
     };
 
