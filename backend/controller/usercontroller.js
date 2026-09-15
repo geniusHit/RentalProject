b8c472d829c580e739dea5cf9a9c0e48096b1ae6
geniusHit <rohitthakur792002@gmail.com>
2026-09-15 22:09:51 +0530

    Error handling

--- backend/controller/usercontroller.js
+++ backend/controller/usercontroller.js
@@ -34,44 +34,55 @@ const users = mongoose.Schema({
 })
 const usersModel = mongoose.model("users", users)
 exports.sendSignupOtp = async (req, res) => {
-    let a = Math.random()
-    a = Math.ceil(a * 999999)
+    try {
+        let a = Math.random()
+        a = Math.ceil(a * 999999)
 
-    const auth = nodemailer.createTransport({
-        service: "gmail",
-        secure: true,
-        port: 465,
-        auth: {
-            user: "rohitthakur792002@gmail.com",
-            pass: "omzd rsxw zwql xvrb"
+        const auth = nodemailer.createTransport({
+            service: "gmail",
+            secure: true,
+            port: 465,
+            auth: {
+                user: "rohitthakur792002@gmail.com",
+                pass: "omzd rsxw zwql xvrb"
+            }
+        })
+
+        const receiver = {
+            from: "rohitthakur792002@gmail.com",
+            to: `${req.body?.email}`,
+            subject: `Team Rental Items. Signup otp.`,
+            html: `Your Otp is ${a}`
         }
-    })
 
-    const receiver = {
-        from: "rohitthakur792002@gmail.com",
-        to: `${req.body?.email}`,
-        subject: `Team Rental Items. Signup otp.`,
-        html: `Your Otp is ${a}`
+        auth.sendMail(receiver, (error, emailResponse) => {
+            if (error) {
+                throw error;
+                return;
+            }
+            console.log("success!")
+            res.status(200).json({ signup_otp: a })
+        })
     }
-
-    auth.sendMail(receiver, (error, emailResponse) => {
-        if (error) {
-            throw error;
-            return;
-        }
-        console.log("success!")
-        res.send({ signup_otp: a })
-    })
+    catch (err) {
+        res.status(400).json({ success: false, message: `Unable to send signup otp. ${err.message}` })
+    }
 }
 
 exports.addUser = async (req, res) => {
-    const saltRounds = 10;
-    console.log("req.body : ", req.body)
-    // const hashedPassword = await bcrypt.hash(req.body.password, saltRounds)
-    const user = await new usersModel(req.body)
-    const result = await user.save();
+    try {
+        const saltRounds = 10;
+        console.log("req.body : ", req.body)
+        // const hashedPassword = await bcrypt.hash(req.body.password, saltRounds)
+        const user = await new usersModel(req.body)
+        const result = await user.save();
 
-    res.send(result)
+        res.status(200).json(result)
+    }
+    catch (err) {
+        res.status(400).json({ success: false, message: `Unable to add user. ${err.message}` })
+    }
+
 }
 
 const products = mongoose.Schema({
@@ -120,38 +131,47 @@ const products = mongoose.Schema({
 })
 const productsModel = mongoose.model("product", products)
 exports.addProduct = async (req, res) => {
-    const product = await new productsModel(req.body)
-    const result = await product.save()
+    try {
+        const product = await new productsModel(req.body)
+        const result = await product.save()
 
-    res.status(200).json({
-        success: true
-    })
+        res.status(200).json({
+            success: true
+        })
+    }
+    catch(err){
+        res.status(200).json({ success: false, message: `Unable to add product. ${err.message}` })
+    }
 }
 
 exports.getProducts = async (req, res) => {
-    const result = await productsModel.find({})
-
-    res.status(200).json(result)
+    try {
+        const result = await productsModel.find({})
+        res.status(200).json(result)
+    }
+    catch (err) {
+        res.status(200).json({ success: false, message: `Unable to get products. ${err.message}` })
+    }
 }
 
 exports.loginUser = async (req, res) => {
     try {
         console.log("req.body : ", req.body)
-        const deletePreviousLogins = await loggedUsersModel.deleteMany({IP: req.body.IP})
+        const deletePreviousLogins = await loggedUsersModel.deleteMany({ IP: req.body.IP })
         const user = await usersModel.findOne({ email: req.body.email, password: req.body.password })
         console.log("user : ", user)
         if (user?.email) {
-            const newLoggedUser = await new loggedUsersModel({email: req.body.email, IP: req.body.IP, expiry: req.body.expiry})
+            const newLoggedUser = await new loggedUsersModel({ email: req.body.email, IP: req.body.IP, expiry: req.body.expiry })
             await newLoggedUser.save()
-            
+
             res.json({ ...newLoggedUser, success: true })
         }
-        else{
+        else {
             res.json({ success: false })
         }
     }
     catch (err) {
-        return res.status(400).json({ success: false, message: err.message })
+        return res.status(400).json({ success: false, message: `Unable to login. ${err.message}` })
     }
 }
 
@@ -235,155 +255,185 @@ const rentalItemsSchema = mongoose.Schema({
 })
 const rentalItems = mongoose.model("rentalItems", rentalItemsSchema)
 exports.addRentalItems = async (req, res) => {
-    const { email, name, userName, sku, quantity } = req.body;
-    const user = await usersModel.findOne({ email: email })
-    const existingProduct = await rentalItems.findOne({ "user.email": email, "user.name": userName, name: name });
-    if (existingProduct) {
-        res.send({ message: "Product is already in Rental Items!" });
-        return;
-    }
-
-    let expiryDate = new Date();
-    expiryDate.setDate(expiryDate.getDate() + 1);
-    const data = req.body;
-    const data2 = {
-        name: data.name,
-        imageNames: data.imageNames,
-        sku: data.sku,
-        condition: data.condition,
-        brand: data.brand,
-        material: data.material,
-        description: data.description,
-        price: data.price,
-        quantity: data.quantity,
-        deliveryCharge: data.deliveryCharge,
-        returnPolicy: data.returnPolicy,
-        assemblyRequired: data.assemblyRequired,
-        notes: data.notes,
-        rentDays: data.rentDays,
-        rentedDate: new Date(),
-        deliveryStatus: "PENDING",
-    }
-    const item = new rentalItems({
-        ...data2, user: {
-            name: userName,
-            email: email,
-            pincode: user.pincode,
-            address: user.address
+    try {
+        const { email, name, userName, sku, quantity } = req.body;
+        const user = await usersModel.findOne({ email: email })
+        const existingProduct = await rentalItems.findOne({ "user.email": email, "user.name": userName, name: name });
+        if (existingProduct) {
+            res.send({ message: "Product is already in Rental Items!" });
+            return;
         }
-    })
-    const result = await item.save()
 
-    const updateQuantity = await productsModel.findOneAndUpdate({ sku: sku }, { quantity: (quantity - 1) })
+        let expiryDate = new Date();
+        expiryDate.setDate(expiryDate.getDate() + 1);
+        const data = req.body;
+        const data2 = {
+            name: data.name,
+            imageNames: data.imageNames,
+            sku: data.sku,
+            condition: data.condition,
+            brand: data.brand,
+            material: data.material,
+            description: data.description,
+            price: data.price,
+            quantity: data.quantity,
+            deliveryCharge: data.deliveryCharge,
+            returnPolicy: data.returnPolicy,
+            assemblyRequired: data.assemblyRequired,
+            notes: data.notes,
+            rentDays: data.rentDays,
+            rentedDate: new Date(),
+            deliveryStatus: "PENDING",
+        }
+        const item = new rentalItems({
+            ...data2, user: {
+                name: userName,
+                email: email,
+                pincode: user.pincode,
+                address: user.address
+            }
+        })
+        const result = await item.save()
 
-    res.send({ message: "Product added to Rental Items" })
+        const updateQuantity = await productsModel.findOneAndUpdate({ sku: sku }, { quantity: (quantity - 1) })
+
+        res.json({ message: "Product added to Rental Items" })
+    }
+    catch (err) {
+        res.status(400).json({ success: false, message: `Unable to add rental items. ${err.message}` })
+    }
 }
 
 exports.myRentalItems = async (req, res) => {
-    const { name, email } = req.body;
-    const items = await rentalItems.find({ "user.email": email })
+    try {
+        const { name, email } = req.body;
+        const items = await rentalItems.find({ "user.email": email })
 
-    res.send(items)
+        res.json(items)
+    }
+    catch (err) {
+        res.status(400).json({ success: false, message: `Unable to get my rental items. ${err.message}` })
+    }
 }
 
 exports.allRentals = async (req, res) => {
-    const allR = await rentalItems.find({})
-    res.send(allR)
+    try {
+        const allR = await rentalItems.find({})
+        res.send(allR)
+    }
+    catch (err) {
+        res.status(400).json({ success: false, message: `Unable to get rental items. ${err.message}` })
+    }
 }
 
 exports.searchProducts = async (req, res) => {
-    const searchKey = req.body.search;
-    const products = await productsModel.find({
-        $or: [
-            { name: { $regex: `${searchKey}`, $options: 'i' } },
-            { brand: { $regex: `${searchKey}`, $options: 'i' } },
-            { condition: { $regex: `${searchKey}`, $options: 'i' } },
-            { description: { $regex: `${searchKey}`, $options: 'i' } },
-            { notes: { $regex: `${searchKey}`, $options: 'i' } },
-            { category: { $regex: `${searchKey}`, $options: 'i' } },
-        ]
-    });
+    try {
+        const searchKey = req.body.search;
+        const products = await productsModel.find({
+            $or: [
+                { name: { $regex: `${searchKey}`, $options: 'i' } },
+                { brand: { $regex: `${searchKey}`, $options: 'i' } },
+                { condition: { $regex: `${searchKey}`, $options: 'i' } },
+                { description: { $regex: `${searchKey}`, $options: 'i' } },
+                { notes: { $regex: `${searchKey}`, $options: 'i' } },
+                { category: { $regex: `${searchKey}`, $options: 'i' } },
+            ]
+        });
 
-    res.send(products)
+        res.send(products)
+    }
+    catch (err) {
+        res.status(400).json({ success: false, message: `Unable to search products. ${err.message}` })
+    }
 }
 
 exports.deliverItem = async (req, res) => {
-    const { name, email, city, address } = req.body.user;
-    const data = req.body
-    const updatedData = { ...data, deliveryStatus: "DELIVERED" }
+    try {
+        const { name, email, city, address } = req.body.user;
+        const data = req.body
+        const updatedData = { ...data, deliveryStatus: "DELIVERED" }
 
-    const updateRentalItems = await rentalItems.findByIdAndUpdate(data._id,
-        { $set: updatedData }
-    )
+        const updateRentalItems = await rentalItems.findByIdAndUpdate(data._id,
+            { $set: updatedData }
+        )
 
-    const auth = nodemailer.createTransport({
-        service: "gmail",
-        secure: true,
-        port: 465,
-        auth: {
-            user: "rohitthakur792002@gmail.com",
-            pass: "omzd rsxw zwql xvrb"
+        const auth = nodemailer.createTransport({
+            service: "gmail",
+            secure: true,
+            port: 465,
+            auth: {
+                user: "rohitthakur792002@gmail.com",
+                pass: "omzd rsxw zwql xvrb"
+            }
+        })
+        const receiver = {
+            from: "rohitthakur792002@gmail.com",
+            to: `${email}`,
+            subject: `Team Rental Items. Item ${req.body.name} has been shipped to your provided address.`,
+            html: `<b>Hello</b> ${name}. Your ordered item ${req.body.name} has been shipped to your provided address ${address}. It will be delived in 5 days.`
         }
-    })
-    const receiver = {
-        from: "rohitthakur792002@gmail.com",
-        to: `${email}`,
-        subject: `Team Rental Items. Item ${req.body.name} has been shipped to your provided address.`,
-        html: `<b>Hello</b> ${name}. Your ordered item ${req.body.name} has been shipped to your provided address ${address}. It will be delived in 5 days.`
-    }
 
-    auth.sendMail(receiver, (error, emailResponse) => {
-        if (error)
-            throw error;
-        console.log("success!")
+        auth.sendMail(receiver, (error, emailResponse) => {
+            if (error)
+                throw error;
+            console.log("success!")
+            res.end()
+        })
+
         res.end()
-    })
-
-    res.end()
+    }
+    catch (err) {
+        res.status(400).json({ success: false, message: `Item not delivered. ${err.message}` })
+    }
 }
 
 exports.createTestPaymentLink = async (req, res) => {
-    console.log("req.body : ", req.body)
-    const url = "https://sandbox.cashfree.com/pg/links";
-    const linkId = `link_${Date.now()}`;
-    const { email, } = req.body;
-    const user = await usersModel.findOne({ email: email })
+    try {
+        console.log("req.body : ", req.body)
+        const url = "https://sandbox.cashfree.com/pg/links";
+        const linkId = `link_${Date.now()}`;
+        const { email, } = req.body;
+        const user = await usersModel.findOne({ email: email })
 
-    const payload = {
-        link_id: linkId,
-        link_amount: Number(req.body.price),
-        link_currency: "INR",
-        link_purpose: "Test payment for project",
-        customer_details: {
-            customer_phone: `1234567890`,
-            customer_email: `${user?.email}`,
-            customer_name: `${user?.name}`
-        },
-        link_notify: {
-            send_sms: false,
-            send_email: false
-        },
-        link_meta: {
-            return_url: `https://rental-project-seven.vercel.app/catalog`,
-            // return_url: `http://localhost:5173/catalog`
-        }
-    };
+        const payload = {
+            link_id: linkId,
+            link_amount: Number(req.body.price),
+            link_currency: "INR",
+            link_purpose: "Test payment for project",
+            customer_details: {
+                customer_phone: `1234567890`,
+                customer_email: `${user?.email}`,
+                customer_name: `${user?.name}`
+            },
+            link_notify: {
+                send_sms: false,
+                send_email: false
+            },
+            link_meta: {
+                return_url: `https://rental-project-seven.vercel.app/catalog`,
+                // return_url: `http://localhost:5173/catalog`
+            }
+        };
 
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "x-api-version": "2023-08-01",
-            "x-client-id": process.env.CASHFREE_CLIENT_ID,
-            "x-client-secret": process.env.CASHFREE_SECRET_KEY
-        },
-        body: JSON.stringify(payload)
-    });
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "x-api-version": "2023-08-01",
+                "x-client-id": process.env.CASHFREE_CLIENT_ID,
+                "x-client-secret": process.env.CASHFREE_SECRET_KEY
+            },
+            body: JSON.stringify(payload)
+        });
 
-    const data = await response.json();
-    const token = jwt.sign({ ...req.body, link_id: data.link_id }, process.env.JWT_SECRET, { expiresIn: "1h" })
+        const data = await response.json();
+        const token = jwt.sign({ ...req.body, link_id: data.link_id }, process.env.JWT_SECRET, { expiresIn: "1h" })
 
-    res.send({ ...data, payment_token: token })
+        res.send({ ...data, payment_token: token })
+    }
+    catch (err) {
+        res.status(400).json({ success: false, message: `Unable to create payment link. ${err.message}` })
+    }
 }
 
 exports.verifyPaymentLink = async (req, res) => {
@@ -402,9 +452,9 @@ exports.verifyPaymentLink = async (req, res) => {
 
         const data = await response.json();
 
-        res.send(data)
+        res.json(data)
     } catch (error) {
-        return res.status(500).json({ success: false, message: error.message });
+        return res.status(500).json({ success: false, message: `Unable to verify payment. ${error.message}` });
     }
 };
 
@@ -419,10 +469,10 @@ exports.updateInventory = async (req, res) => {
             );
         })
 
-        res.send({ message: "Inventory has been updated." })
+        res.json({ message: "Inventory has been updated." })
     }
     catch (err) {
-        return res.status(400).json({ success: false, message: err.message })
+        return res.status(400).json({ success: false, message: `Unable to update inventory. ${err.message}` })
     }
 }
 
@@ -451,12 +501,17 @@ const loggedUsers = mongoose.Schema({
 const loggedUsersModel = mongoose.model("loggedUsers", loggedUsers)
 
 exports.getLoginUser = async (req, res) => {
-    console.log("req.body.IP : ", req.body.IP)
-    const loggedUser = await loggedUsersModel.findOne({ IP: req.body.IP })
-    console.log("loggedUser : ", loggedUser)
-    const user = loggedUser !== null && await usersModel.findOne({ email: loggedUser.email })
+    try {
+        console.log("req.body.IP : ", req.body.IP)
+        const loggedUser = await loggedUsersModel.findOne({ IP: req.body.IP })
+        console.log("loggedUser : ", loggedUser)
+        const user = loggedUser !== null && await usersModel.findOne({ email: loggedUser.email })
 
-    res.send({ loggedUser: loggedUser, user: user })
+        res.send({ loggedUser: loggedUser, user: user })
+    }
+    catch (err) {
+        return res.status(400).json({ success: false, message: `Unable to get login user. ${err.message}` })
+    }
 }
 
 
@@ -467,22 +522,35 @@ const paymentToken = mongoose.Schema({
 })
 const paymentTokenModel = mongoose.model("paymentToken", paymentToken)
 exports.savePaymentToken = async (req, res) => {
-    const token = await new paymentTokenModel(req.body)
-    await token.save()
+    try {
+        const token = await new paymentTokenModel(req.body)
+        await token.save()
 
-    res.send("Payment token saved successfully.")
+        res.send("Payment token saved successfully.")
+    }
+    catch (err) {
+        return res.status(400).json({ success: false, message: `Unable to save payment token. ${err.message}` })
+    }
 }
 
 exports.getPaymentToken = async (req, res) => {
-    const token = await paymentTokenModel.find({})
-
-    res.send(token)
+    try {
+        const token = await paymentTokenModel.find({})
+        res.send(token)
+    }
+    catch (err) {
+        return res.status(400).json({ success: false, message: `Unable to get payment token. ${err.message}` })
+    }
 }
 
 exports.deletePaymentToken = async (req, res) => {
-    const deleteToken = await paymentTokenModel.deleteMany({})
-
-    res.send(deleteToken)
+    try {
+        const deleteToken = await paymentTokenModel.deleteMany({})
+        res.send(deleteToken)
+    }
+    catch (err) {
+        return res.status(400).json({ success: false, message: `Unable to delete item. ${err.message}` })
+    }
 }
 
 exports.logout = async (req, res) => {
