602db992446bbdbd7c08342a0857ae0e35c7bd3a
geniusHit <rohitthakur792002@gmail.com>
2026-09-18 19:47:16 +0530

    Vercel deploy

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,10 @@ const connection = require("./connection/database.js");
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(cookieParser())
 
 app.get("/", (req, res) => {
     res.status(200).json({
